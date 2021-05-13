dc1f3592eceb3d879a82658c90e6d643ff5b37ad
Manik Ray <manikchandraray229@gmail.com>
2021-05-14 01:29:15 +0600

    add code

---
+++ nullUndefined.js
@@ -0,0 +1,21 @@
+// function add (num1, num2){
+//     console.log(num1 + num2);
+
+
+// }
+
+// const result = add(12, 88);
+// console.log(result);
+
+// function add (num1 , num2){
+//     const sum = num1 + num2;
+//     return sum;
+// }
+
+// const result = add(12, 88);
+// console.log(result);
+
+
+const name = {name:"Manik", phone: 546464 }
+
+console.log(name.gf);
