014a61950e074fb0beed98275d71ef2bcc9b9b48
Manik Ray <manikchandraray229@gmail.com>
2021-05-14 15:52:48 +0600

    onek kichu siklam

---
+++ arraySplit.js
@@ -0,0 +1,10 @@
+const nums = [4, 8, 4, 5, 3, 8, 87, 6,];
+// const part = nums.slice(1, 5);
+
+const remove = nums.splice(2, 5);
+
+
+const together = nums.join(',')
+
+console.log(together);
+console.log(nums);
